dbf6032b17dc351b1dae2a2d09c7baa389300c5a
koustubh-dwivedy <koustubhdwivedy@gmail.com>
2017-09-07 20:24:20 +0530

    first iteration sim ready

--- F_1.m
+++ F_1.m
@@ -6,17 +6,22 @@
 % y(6)-> angle
 function vec = F_1(t,y)
 
-	c = 0;
+    global c;
+    global m;
+    global d;
+    global L;
+	
+    %c = 0;
 
-	m = 0.15;
+	%m = 1;
 	I = 1.46*(10^(-4));
 	x_cm = 0.0542;
-	L = 0.108;
-	d = 0.0150;
+	%L = 0.108;
+	%d = 0.0150;
 	r = d/2;
 	rho = 1000;
 	vap_pressure = 2.5*(10^3);
-	pressure = rho*9.8*1; %at 1m depth
+	pressure = rho*9.8*20; %at 1m depth
 
 	k = 0.82;
 	K = 2*pi;
